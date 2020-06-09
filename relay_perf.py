bbccf37501a7bad2abff17cb5b204258093a9dd7
Dennis Jackson <ommitted>
2020-06-09 18:51:51 +0100

    Rewrite to use sqlite

--- relay_perf.py
+++ relay_perf.py
@@ -11,90 +11,102 @@ from twisted.internet.defer import ensureDeferred
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet.task import react
 
-def write_json(filestem, data):
-    now = datetime.datetime.now().strftime("%Y%m%d_%H%M");
-    #print(data)
-    jsonStr = json.dumps(data)
-    with open(filestem + "_" + now + ".json", "w") as f:
-        f.write(jsonStr)
-    with open(filestem + "_latest.json", "w") as f:
-        f.write(jsonStr)
+from peewee import *
 
-def relay_data(exits):
-    url = "https://onionoo.torproject.org/details?type=relay&fields=nickname,fingerprint,as_name,country_name,contact,platform,or_addresses,bandwidth_rate,exit_probability"
-    if exits:
-        url += "&flag=exit"
-    req = urllib.request.Request(url)
-    response = urllib.request.urlopen(req).read()
-    data = json.loads(response.decode('utf-8'))
-    return data["relays"]
+db = SqliteDatabase('measurements.db')
+
+
+class Measurement(Model):
+    t_measure = DateTimeField()
+    guard = TextField()
+    exit = TextField()
+    url = TextField()
+
+    circuit_success = BooleanField()
+    circuit_t_start = DateTimeField()
+    circuit_t_stop = DateTimeField()
+    circuit_error = CharField()
+
+    request_success = BooleanField()
+    request_t_start = DateTimeField()
+    request_t_stop = DateTimeField()
+    request_error = CharField()
+
+    class Meta:
+        database = db
+
 
 async def launch_tor(reactor):
     control_ep = TCP4ClientEndpoint(reactor, "localhost", 9051)
-    tor = await txtorcon.connect(reactor, control_ep, password_function = lambda: "bilboBaggins789")
-    #tor = await txtorcon.launch(reactor, progress_updates=print, data_directory="./tor_data")
+    tor = await txtorcon.connect(reactor, control_ep, password_function=lambda: "bilboBaggins789")
+    # tor = await txtorcon.launch(reactor, progress_updates=print, data_directory="./tor_data")
     config = await tor.get_config()
     state = await tor.create_state()
     socks = await config.create_socks_endpoint(reactor, "9050")
     print("Connected to tor {}".format(tor.version))
     return [tor, config, state, socks]
 
+
 async def build_two_hop_circuit(state, guard, exit_node):
     circuit = {}
     success = None
     error = ""
-    t_start = time.time()
+    t_start = datetime.datetime.now()
     try:
-        circuit = await state.build_circuit(routers = [guard, exit_node], using_guards = False)
+        circuit = await state.build_circuit(routers=[guard, exit_node], using_guards=False)
         await circuit.when_built()
         success = True
     except Exception as err:
         error = str(err)
         success = False
-    t_stop = time.time()
-    return { "circuit" : circuit,
-             "success" : success,
-             "delta" : t_stop - t_start,
-             "error" : error }
+    t_stop = datetime.datetime.now()
+    return circuit, {"t_built": circuit.time_created,
+                     "success": success,
+                     "t_start": t_start,
+                     "t_stop": t_stop,
+                     "error": error}
+
 
 async def request_over_circuit(reactor, socks, circuit, bareIP):
     success = None
     error = ""
-    t_start = time.time()
+    t_start = datetime.datetime.now()
+    url = b"http://93.184.216.34" if bareIP else b"http://example.com"
     try:
         agent = circuit.web_agent(reactor, socks)
-        resp = await agent.request(b'HEAD', b"http://93.184.216.34" if bareIP else b"http://example.com")
+        resp = await agent.request(b'HEAD', url)
         success = True
     except Exception as err:
         error = str(err)
         success = False
-    t_stop = time.time()
-    return { "success" : success,
-             "delta" : t_stop - t_start,
-             "error" : error }
+    t_stop = datetime.datetime.now()
+    return {"success": success,
+            "t_start": t_start,
+            "t_stop": t_stop,
+            "error": error,
+            "url": url}
+
 
 async def time_two_hop(reactor, state, socks, guard, exit_node, bareIP):
-    circuit_results = await build_two_hop_circuit(state, guard, exit_node)
+    timestamp = datetime.datetime.now()
+    circuit, circuit_results = await build_two_hop_circuit(state, guard, exit_node)
     if circuit_results["success"]:
-        request_results = await request_over_circuit(reactor, socks, circuit_results["circuit"], bareIP)
-        return { "delta_request": request_results["delta"],
-                 "delta_circuit": circuit_results["delta"],
-                 "result": ("SUCCEEDED" if request_results["success"] else ("Request error: " + request_results["error"])) }
+        request_results = await request_over_circuit(reactor, socks, circuit, bareIP)
     else:
-        return { "delta_request": -1,
-                 "delta_circuit": circuit_results["delta"],
-                 "result": ("Circuit error: " + circuit_results["error"]) }
+        request_results = {"success": False,
+            "t_start": None,
+            "t_stop": None,
+            "error": "NO_CIRCUIT",
+            "url": None}
+    measurement = {"t_measure": timestamp,
+                   "guard": guard.id_hex,
+                   "exit": exit_node.id_hex,
+                   "circuit": circuit_results,
+                   "request": request_results}
+    return measurement
 
-def record_result(results, fingerprint, address, result, delta):
-    if address not in results:
-        results[address] = {}
-    if fingerprint not in results[address]:
-        results[address][fingerprint] = []
-    dateString = str(datetime.datetime.now())
-    results[address][fingerprint].append((result, dateString, delta))
 
 async def test_relays(reactor, state, socks, relays, exits, repeats, bareIP):
-    results = {}
     nr = len(relays)
     ne = len(exits)
     n = nr * ne
@@ -104,11 +116,23 @@ async def test_relays(reactor, state, socks, relays, exits, repeats, bareIP):
             for exit_node in exits:
                 j = j + 1
                 result = await time_two_hop(reactor, state, socks, relay, exit_node, bareIP)
-                relay_key = relay.id_hex if (nr > 1) else exit_node.id_hex
-                record_result(results, relay_key, "example.com", result["result"], result["delta_request"])
-                print('%d/%d: %d/%d' % (i+1, repeats, j, n),
-                      relay.id_hex, "->", exit_node.id_hex, ":", result)
-    return results
+                print(result)
+                m = Measurement(t_measure=result['t_measure'],
+                                guard=result['guard'],
+                                exit=result['exit'],
+                                url=result['request']['url'],
+                                circuit_success=result['circuit']['success'],
+                                circuit_t_start=result['circuit']['t_start'],
+                                circuit_t_stop=result['circuit']['t_stop'],
+                                circuit_error=result['circuit']['error'],
+                                request_success = result['request']['success'],
+                                request_t_start = result['request']['t_start'],
+                                request_t_stop = result['request']['t_stop'],
+                                request_error = result['request']['error']
+                )
+                m.save()
+    return None
+
 
 async def _main(reactor, fingerprint, bareIP):
     [tor, config, state, socks] = await launch_tor(reactor)
@@ -124,14 +148,12 @@ async def _main(reactor, fingerprint, bareIP):
     guard1 = state.routers_by_hash["$6C251FA7F45E9DEDF5F69BA3D167F6BA736F49CD"]
     exits = list(filter(lambda router: "exit" in router.flags, routers))
     exit_results = await test_relays(reactor, state, socks, [guard1], exits, 10, bareIP)
-    exit_results["_relays"] = relay_data(True)
-    write_json("../all_exit_results/exit_results", exit_results)
+    print(exit_results)
 
     exit_node = state.routers_by_hash["$7BD7B547676257EF147F5D5B7A5B15F840F4B579"]
     relays = list(filter(lambda router: "exit" not in router.flags, routers))
     relay_results = await test_relays(reactor, state, socks, relays, [exit_node], 3, False)
-    relay_results["_relays"] = relay_data(False)
-    write_json("../all_relay_results/relay_results", relay_results)
+
 
 def main(fingerprint, bareIP):
     return react(
@@ -140,5 +162,9 @@ def main(fingerprint, bareIP):
         )
     )
 
+
 if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Measurement])
     main(None, False)
+    db.close()
